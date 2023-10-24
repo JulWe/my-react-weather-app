00c091b9e109cdc83bd9f11c031febd6e86dca1a
JulWe <132477835+JulWe@users.noreply.github.com>
2023-10-24 15:43:22 +0200

    added the footer and added rel to the link

--- src/App.js
+++ src/App.js
@@ -5,11 +5,17 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
-      <p>
+      <footer>
         This project was coded by Julia and is{" "}
-        <a href="https://github.com/JulWe/my-react-weather-app" target="_blank">open sourced</a>
+        <a
+          href="https://github.com/JulWe/my-react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open sourced
+        </a>
         .
-      </p>
+      </footer>
     </div>
   );
 }
