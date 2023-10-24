ef7ba1541a0ea4316740ed4b7e47b652c7701471
JulWe <132477835+JulWe@users.noreply.github.com>
2023-10-24 15:41:02 +0200

    added the link to my GitHub repository

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <p>
+        This project was coded by Julia and is{" "}
+        <a href="https://github.com/JulWe/my-react-weather-app" target="_blank">open sourced</a>
+        .
+      </p>
     </div>
   );
 }
